60d4714a2604881afe865a37b46f12d4f6fc3b7b
Jordan Squire <jordansquire@gmail.com>
2014-08-29 10:26:44 -0600

    Admin login fix

--- participate_old.php
+++ participate_old.php
@@ -5,15 +5,7 @@
   $request = $_SERVER['REQUEST_METHOD'];
   
   if ($request == 'POST') {
-    if (isset($_POST['password']) && strtolower($_POST['password']) == "ogden") {
-      if (isset($_POST['stake']) && isValidStake($_POST['stake'])) {
-        setcookie('logged_in','cmsyemckjfdaoiisa',time() + (86400 * 120));
-        setcookie('stake',$_POST['stake'],time() + (86400 * 120));
-        header('Location: choir.php');
-      } else {
-        $badstake = 1;
-      }
-    } elseif (isset($_POST['password']) && strtolower($_POST['password']) == "@dmin") {
+    if (isset($_POST['password']) && strtolower($_POST['password']) == "@dmin") {
       setcookie('logged_in','cmsyemckjfdaoiisa',time() + (86400 * 120));
       setcookie('stake','ADMIN',time() + (86400 * 120));
       header('Location: choir.php');
@@ -30,67 +22,25 @@
 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
 <script src="//code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
 
-<h1>Participate in the Ogden Temple<br/><br/>Cultural Celebration</h1>
-<div class="divider"></div>
-<h3>Shine the Light</h3> 
-<p>Attention all youth 12-18 in the Ogden Utah Temple District. The cultural celebration is a chance for you 
-  to share your testimonies with your friends and family and those you don't know, by providing a spiritual 
-  experience through singing and dancing, in celebrating the blessings of the Lord's house.</p>
-<p>Everyone who would like to participate will be welcomed. 
-  <ul>
-    <li>Sing in the 2000 voice choir</li>
-    <li>Perform musical numbers - no dance background required</li>
-    <li>Audition for the Dance Corps and learn more advance choreography
-      <ul>
-        <li>Audition Date: July 12 at 9 am</li>
-        <li>Place: Sunset North Church Building, 2431 N 250 W, Sunset UT 84015</li>
-      </ul>  
-    </li>
-  </ul>
-  Contact your local bishop to find out ways you can be part of this once in a lifetime opportunity. Not 
-  sure who your bishop is? Enter your address <a href="http://www.mormon.org/meetinghouse">here</a>.</p>
-<p><b>All participants will need to complete a <a href="files/Release_to_Use_Image.pdf">release form</a> and 
-  <a href="files/parental-permission-medical-release.pdf">permission slip</a></b>.</p>
-<br/>
+<h1>Admin Login</h1>
 <a name="login"></a>
 <div class="form">
   <?php
     if (!isset($_COOKIE['logged_in'])) {
   ?>
-    <script>
-    $(function() {
-      var availableTags = [
-      <?php 
-       $stakes = getStakes();
-       foreach ($stakes as $idx => $stake) {
-         if($idx != 0) {
-            echo ",\n";
-          }
-         echo "\"".$stake->name."\"";
-       }
-      ?>
-      ];
-      $( "#stake" ).autocomplete({
-        source: availableTags
-      });
-    });
-    </script>
     <div class="left_half">
       <h3>Login</h3>
-      <p>Select your stake and login with the password you have been provided to download music, choreography, and other information.</p>
+      <p>Enter the admin password.</p>
     </div>
     <div class="right_half">
-      <form action="participate.php" method="post">
+      <form action="participate_old.php" method="post">
         <?php
           if (isset($badlogin)) {
             echo '<span class="error">The password is incorrect.</span><br />';
           } elseif (isset($login_required)) {
             echo '<span class="error">Login required.</span><br />';
-          } elseif (isset($badstake)) {
-            echo '<span class="error">You must select a valid Stake from the list.</span><br />';
           }
         ?>
-        <input name="stake" id="stake" placeholder='Type the name of your Stake' /><br/>
         <input name="password" id="password" type="password" placeholder='Password' /><br/>
         <input type="submit" id="login" value="Login" />
       </form>
